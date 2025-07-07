2703ba4ecd01c3eab4abf34ce701fd73ba82e2cd
NonEntity <derek.herman@gmail.com>
2025-07-06 22:54:31 -0600

    Vendor reedsolo and update package init

--- ccsds_tmtc_py/__init__.py
+++ ccsds_tmtc_py/__init__.py
@@ -1,1 +1,9 @@
+# Ensure bundled third-party modules are importable
+import os
+import sys
+
+_VENDOR_DIR = os.path.join(os.path.dirname(__file__), "..", "vendor")
+if os.path.isdir(_VENDOR_DIR) and _VENDOR_DIR not in sys.path:
+    sys.path.insert(0, _VENDOR_DIR)
+
 # This file marks ccsds_tmtc_py as a Python package.
